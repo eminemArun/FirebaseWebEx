a9a93905830dba1344c94861ac2059a053f02729
ArunNegi <negi.arun@tcs.com>
2018-12-26 16:24:35 +0100

    Added new config files

--- FireBase/no/app.js
+++ FireBase/no/app.js
@@ -6,18 +6,35 @@ function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
     let desc = document.createElement('span');
-    let weblink = document.createElement('span');
+    //let weblink = document.createElement('span');
+    //let imageurl = document.createElement('span');
     let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     desc.textContent = doc.data().desc;
-    weblink.textContent = doc.data().weblink;
+   // weblink.textContent = doc.data().weblink;
+    //imageurl.textContent = doc.data().imageurl;
     cross.textContent = 'x';
+
+
+    var imagelink = doc.data().imageurl;
+    var element = document.createElement("a");
+    element.setAttribute("href", imagelink);
+    element.innerHTML = "Image Link";
+
+    var weblink = doc.data().weblink;
+    var webelement = document.createElement("a");
+    webelement.setAttribute("href", weblink);
+    webelement.innerHTML = "website Link";
+// and append it to where you'd like it to go:
 
     li.appendChild(name);
     li.appendChild(desc);
-    li.appendChild(weblink);
+    //li.appendChild(weblink);
+    li.appendChild(webelement);
+     li.appendChild(element);
+    //li.appendChild(imageurl);
     li.appendChild(cross);
 
     cafeList.appendChild(li);
@@ -39,16 +56,22 @@ function renderCafe(doc){
 
 // saving data
 form.addEventListener('submit', (e) => {
+
+    if (form.name.value.length == 0 || form.desc.value.length == 0 || form.weblink.value.length == 0) {
+     alert("Require Filed are empty"); return false;
+    }
     e.preventDefault();
     db.collection('Teas').add({
         name: form.name.value,
         desc: form.desc.value,
-        weblink: form.weblink.value
+        weblink: form.weblink.value,
+        imageurl: form.imageurl.value
 
     });
     form.name.value = '';
     form.desc.value = '';
      form.weblink.value = '';
+    form.imageurl.value = '';
 });
 
 // real-time listener
---
+++ FireBase/tea/README.md
@@ -0,0 +1,1 @@
+New in Web Technology so just playing around it and saving,getting data from firebase
